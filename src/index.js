3a079a5668f365188af1a6c06cf674b13bec7a25
Sarthak Kumar <sarthakk145@gmail.com>
2024-10-22 00:18:42 +0530

    import images from directory to the webapp

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './style.css';
+import image1 from './Levi_Ackerman_character_image_29.webp'
+import image2 from './Levi_thanks_his_friends.webp'
+import armin from './Armin_Arlert_character_image.webp'
+import connie from './Connie_Springer_character_image.webp'
+import eren from './Eren_free_from_prison.webp'
+import jean from './Jean_Kirstein_character_image.webp'
+import mikasa from './mikasa.webp'
+import sasha from './sasha.webp'
+
+document.getElementById('levi').src = image1;
+document.getElementById('thank').src = image2;
+document.getElementById('eren').src = eren;
+document.getElementById('armin').src = armin;
+document.getElementById('mikasa').src = mikasa;
+document.getElementById('jean').src = jean;
+document.getElementById('connie').src = connie;
+document.getElementById('sasha').src = sasha;
